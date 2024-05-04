bb9186468ff95c830b88942b4a069c0b24c7c598
Amir Hamza <hamza.com.bd@gmail.com>
2024-05-04 21:02:45 +0600

    Create home, category, single page components and set it's static data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { CategoryPageComponent } from './components/category-page/category-page.component';
+import { SinglePageComponent } from './components/single-page/single-page.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CategoryPageComponent,
+    SinglePageComponent,
+    NavComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
